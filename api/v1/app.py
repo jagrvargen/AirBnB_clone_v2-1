ebf30069dcd1e8c568efec8d9a5da678e2350e5a
jagrvargen <jagarthewolf@gmail.com>
2018-04-24 16:12:09 +0000

    added custom 404 message

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 '''
    This module contains a Flask instance
 '''
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -13,6 +13,11 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 @app.teardown_appcontext
 def teardown_db(self):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
