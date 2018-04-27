e58740590305e00cc98b064851b024eaabb2ed70
magezil <256@holbertonschool.com>
2018-04-27 00:33:36 +0000

    pep8 checks

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -92,8 +92,8 @@ def update_user(user_id):
 
     req = request.get_json()
     for k, v in req.items():
-        if k != "id" and k != "created_at" and k != "updated_at"
-        and k != "email":
+        if k != "id" and k != "created_at" and k != "updated_at" \
+                and k != "email":
             setattr(user, k, v)
     user.save()
 
